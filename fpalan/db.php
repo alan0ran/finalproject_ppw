b1e0a4d1a71ead4b9eb671a66dcd569ea7e4b0aa
alan0ran <89297564+alan0ran@users.noreply.github.com>
2021-12-02 12:07:49 +0700

    Delete fpalan directory

--- fpalan/db.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$servername = "localhost";
-$id = "root";
-$password = "";
-$dbname = "data";
-$mydb = new mysqli($servername, $id, $password, $dbname);
--- fpalan/delete.php
+++
@@ -1,6 +0,0 @@
-<?php
-include 'db.php';
-
-$nim = $_GET['nim'];
-$syntax = mysqli_query($mydb,"DELETE FROM user WHERE nim='$nim'");
-$mydb->query($syntax);
